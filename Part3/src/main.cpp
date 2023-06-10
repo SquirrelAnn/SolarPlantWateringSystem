14d1c3e5a900b936204fbf7f7da009d9adfc96aa
SquirrelAnn <96001954+SquirrelAnn@users.noreply.github.com>
2023-06-10 12:54:34 +0200

    outsource epaper code to separate header file

--- Part3/src/main.cpp
+++ Part3/src/main.cpp
@@ -1,28 +1,13 @@
 #include <Arduino.h>
 
 #include <wdt.h>
+#include <epaper.h>
 
-#include <GxEPD2_BW.h>
-#include <Fonts/FreeMonoBold9pt7b.h>
-
-#define MAX_DISPLAY_BUFFER_SIZE 800
-#define MAX_HEIGHT(EPD) (EPD::HEIGHT <= MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8) ? EPD::HEIGHT : MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8))
-GxEPD2_BW<GxEPD2_290_GDEY029T94, MAX_HEIGHT(GxEPD2_290_GDEY029T94)> display(GxEPD2_290_GDEY029T94(/*CS=*/ SS, /*DC=*/ 8, /*RST=*/ 9, /*BUSY=*/ 7)); // GDEY029T94  128x296, SSD1680, (FPC-A005 20.06.15)
 
 // last time the sensors were measured, in milliseconds
 unsigned long previousMillis = 0;
 // for sensor measuring and possible pump interval
 const long measuringInterval = 18000000; // 5 hour interval
-
-// E paper settings for Arduino Nano or Uno
-// BUSY -> D7
-// RST -> D9
-// DC -> D8
-// CS -> D10
-// CLK -> D13
-// DIN -> D11
-// GND -> GND
-// 3.3V -> 3.3V
 
 // soil moisture sensors
 int Pin1 = A0;
@@ -47,7 +32,7 @@ int measurementNumber = 0;
 void setup() {
   Serial.begin(9600);
 
-  display.init(); 
+  initDisplay();
 
   analogReference(EXTERNAL); // set the analog reference to 3.3V
 
@@ -69,27 +54,6 @@ void setup() {
   setupWatchDogTimer();
 
   delay(500);
-}
-
-void writeToEPaper(const char* msg){
-  display.setRotation(1);
-  display.setFont(&FreeMonoBold9pt7b);
-  display.setTextColor(GxEPD_BLACK);
-  int16_t tbx, tby; uint16_t tbw, tbh;
-  display.getTextBounds(msg, 0, 0, &tbx, &tby, &tbw, &tbh);
-  // center the bounding box by transposition of the origin:
-  uint16_t x = ((display.width() - tbw) / 2) - tbx;
-  uint16_t y = ((display.height() - tbh) / 2) - tby;
-  display.setFullWindow();
-  display.firstPage();
-  do
-  {
-    display.fillScreen(GxEPD_WHITE);
-    display.setCursor(x, y);
-    display.print(msg);
-  }
-  while (display.nextPage());
-  display.hibernate(); // turns powerOff() and sets controller to deep sleep for minimum power use, ONLY if wakeable by RST (rst >= 0)
 }
 
 void water(int pumpNumber)
